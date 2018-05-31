b08c4a5a5620f48f3f7c6a0040ebce97432d0c36
vibs-developer <rakshavibs@gmail.com>
2018-05-31 13:00:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+var express =require("express");
+var app = express();
+var bodyParser = require("body-parser");
+ 
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+var students = [
+    {name: "riya sharma",batch: "12", group: "science"},
+    {name: "siya sharma",batch: "12", group: "commerce"},
+    {name: "priya sharma",batch: "12", group: "science"}
+
+];
+app.get("/", function(req , res){
+        res.render("home");
+}); 
+
+app.get("/students", function(req,res){
+
+        res.render("students",{students:students});
+});
+
+
+app.post("/students",function(req, res){
+       
+    // get data from form and add to students array
+
+   var name = req.body.name;
+    var batch = req.body.batch;
+ var group =  req.body.group;
+ var newStudent = {name:name, batch:batch, group:group}
+ students.push(newStudent);
+
+    // redirect back to students
+    res.redirect("/students");
+
+});
+
+app.get("/students/new", function(req,res){
+    res.render("new.ejs");
+})
+app.listen(3000, function(){
+    console.log("server started")
+})
